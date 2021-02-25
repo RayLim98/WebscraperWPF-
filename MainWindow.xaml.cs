2ade364833273c010b90fdb6835e7fb424da52b3
RaymondLimUTS <raymond.lim@student.uts.edu.au>
2021-02-25 11:12:52 +1100

    commit current task

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebScrapperWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Scraper scraper;
+        public MainWindow()
+        {
+            InitializeComponent();
+            scraper = new Scraper();
+            DataContext = scraper;
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            scraper.getData(url_textbox.Text);
+        }
+    }
+}
---
+++ WebScraper.cs
@@ -0,0 +1,54 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebScrapperWPF
+{
+    public class Scraper
+    {
+        //Contianer of objects
+        //Type EntryModel { Title }
+        private ObservableCollection<EntryModel> _entries = new ObservableCollection<EntryModel>();
+
+        //Gets and Sets Articles that are obtained from scraping website
+        //Used to data bind to a data-grid
+        public ObservableCollection<EntryModel> Entries
+        {
+            get { return _entries; }
+            set { _entries = value; }
+        }
+
+        //Decodes HTML 
+        //Returns String
+        private async Task<string> DecodeHtmlNode(HtmlNode node)
+        {
+            //Decodes 
+            var decodedHtml = await Task.Run(() => HttpUtility.HtmlDecode(node.SelectSingleNode(".//h3").InnerText));
+            return decodedHtml;
+        }
+        //Gets and stores inner text data into ObserableCollection
+        public async Task getData(string page)
+        {
+            var web = new HtmlWeb();
+            //get htmldocument of page
+            var doc = await Task.Run(() => web.Load(page));
+
+            //scrape website" http://books.toscrape.com/, searches for any elements under specified conditions
+            //and reutrns a list of all articles under the same catagory 
+            var Articles = doc.DocumentNode.SelectNodes("//*[@class = 'product_pod']");
+
+            //for each article found 
+            foreach (var article in Articles)
+            {
+                var title = await Task.Run(() => DecodeHtmlNode(article));
+                _entries.Add(new EntryModel { Title = title });
+            }
+        }
+    }
+}
